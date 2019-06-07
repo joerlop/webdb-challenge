ec71b62487678cee4d9c759e469c890b24368ca0
joerlop <jj.erlich155@gmail.com>
2019-06-07 10:27:16 -0500

    Built tables

--- migrations/20190607102001_create_projects_table.js
+++ migrations/20190607102001_create_projects_table.js
@@ -1,8 +1,15 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("projects", function(tbl) {
+    tbl.increments();
+
+    tbl.string("name").notNullable();
+
+    tbl.string("description").notNullable();
+
+    tbl.boolean("completed").notNullable();
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists("projects");
 };
